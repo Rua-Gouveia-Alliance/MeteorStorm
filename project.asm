f76cbe315388400ce4aa580c441002dca6561b68
Joao Gouveia <jjtucki4@gmail.com>
2022-05-26 16:51:47 +0100

    Adaptar o codigo do lab para o circuito projeto
    Os displays do projeto sao de 16 bits e os do lab eram de 8 bits

--- project.asm
+++ project.asm
@@ -1,22 +1,10 @@
-; *********************************************************************
-; * IST-UL
-; * Modulo:    lab3.asm
-; * Descrição: Exemplifica o acesso a um teclado.
-; *            Lê uma linha do teclado, verificando se há alguma tecla
-; *            premida nessa linha.
-; *
-; * Nota: Observe a forma como se acede aos periféricos de 8 bits
-; *       através da instrução MOVB
-; *********************************************************************
 
 ; **********************************************************************
 ; * Constantes
 ; **********************************************************************
-; ATENÇÃO: constantes hexadecimais que comecem por uma letra devem ter 0 antes.
-;          Isto não altera o valor de 16 bits e permite distinguir números de identificadores
-DISPLAYS   EQU 0A000H	; endereço dos displays de 7 segmentos (periférico POUT-1)
-TEC_LIN    EQU 0C000H	; endereço das linhas do teclado (periférico POUT-2)
-TEC_COL    EQU 0E000H	; endereço das colunas do teclado (periférico PIN)
+DISPLAYS   EQU 0A000H	; endereco dos displays de 7 segmentos (periferico POUT-1)
+TEC_LIN    EQU 0C000H	; endereco das linhas do teclado (periferico POUT-2)
+TEC_COL    EQU 0E000H	; endereco das colunas do teclado (periferico PIN)
 LINHA      EQU 16		; linha a testar (comecamos na 4a linha, mas por causa do shift right inicial inicializamos ao dobro)
 MASCARA    EQU 0FH		; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 SHIFTR     EQU 2		; dividir por 2 para fazer o shift right
@@ -26,43 +14,38 @@ SHIFTR     EQU 2		; dividir por 2 para fazer o shift right
 ; **********************************************************************
 PLACE      0
 inicio:
-; inicializações
-    MOV  R2, TEC_LIN	; endereço do periférico das linhas
-    MOV  R3, TEC_COL	; endereço do periférico das colunas
-    MOV  R4, DISPLAYS	; endereço do periférico dos displays
+; inicializacoes
+    MOV  R2, TEC_LIN	; endereco do periferico das linhas
+    MOV  R3, TEC_COL	; endereco do periferico das colunas
+    MOV  R4, DISPLAYS	; endereco do periferico dos displays
 	MOV  R5, MASCARA	; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 	MOV  R6, SHIFTR		; shift right
 
 ; corpo principal do programa
-ciclo:
+main_loop:
 	MOV  R1, 0
-    MOVB [R4], R1		; escreve linha e coluna a zero nos displays
+    MOV [R4], R1		; escreve linha e coluna a zero nos displays
 
-espera_tecla:			; neste ciclo espera-se até uma tecla ser premida
+espera_tecla:			; neste ciclo espera-se ate uma tecla ser premida
 	MOV  R1, LINHA		; comecar por testar a linha 4
-	loop_espera:
-		DIV R1, R6		; shift right
-		CMP R1, 0		; verificar se estamos a testar uma linha valida
-		JZ espera_tecla	; reinicializar o valor da linha e recomecar o ciclo caso linha seja invalida
-		MOVB [R2], R1	; escrever no periférico de saída (linhas)
-		MOVB R0, [R3]	; ler do periférico de entrada (colunas)
-		AND  R0, R5		; elimina bits para além dos bits 0-3
-		CMP  R0, 0		; há tecla premida?
-		JZ  loop_espera	; se nenhuma tecla premida, repete
+loop_espera:
+	DIV R1, R6			; shift right
+	CMP R1, 0			; verificar se estamos a testar uma linha valida
+	JZ espera_tecla		; reinicializar o valor da linha e recomecar o ciclo caso linha seja invalida
+	MOVB [R2], R1		; escrever no periferico de saída (linhas)
+	MOVB R0, [R3]		; ler do periferico de entrada (colunas)
+	AND  R0, R5			; elimina bits para além dos bits 0-3
+	CMP  R0, 0			; ha tecla premida?
+	JZ  loop_espera		; se nenhuma tecla premida, repete
+	MOV R7, R1			; guardar o valor da linha
     SHL  R1, 4			; coloca linha no nibble high
     OR   R1, R0			; junta coluna (nibble low)
-    MOVB [R4], R1		; escreve linha e coluna nos displays
+    MOV [R4], R1		; escreve linha e coluna nos displays
 
-ha_tecla:				; neste ciclo espera-se até NENHUMA tecla estar premida
-    MOV  R1, LINHA		; testar a linha 4  (R1 tinha sido alterado)
-	loop_ha:
-		DIV R1, R6      ; shift right
-		CMP R1, 0       ; verificar se a linha esta a 0
-		JZ ciclo        ; se a linha ta a 0 é porque já verificamos todas e nao ha teclas premidas
-		MOVB [R2], R1   ; escrever no periférico de saída (linhas)
-		MOVB R0, [R3]   ; ler do periférico de entrada (colunas)
-		AND  R0, R5     ; elimina bits para além dos bits 0-3
-		CMP  R0, 0      ; há tecla premida?
-		JNZ  ha_tecla    ; se ainda houver uma tecla premida reinicia as linhas e volta ao inicio do ciclo exteriror
-		JMP loop_ha     ; continua no loop ate todas as linhas terem sido testadas
-
+ha_tecla:				; neste ciclo espera-se ate nenhuma tecla estar premida
+	MOVB [R2], R7		; escrever no periferico de saída (linhas)
+	MOVB R0, [R3]		; ler do periferico de entrada (colunas)
+	AND  R0, R5			; elimina bits para além dos bits 0-3
+	CMP  R0, 0			; ha tecla premida?
+	JNZ  ha_tecla		; se ainda houver uma tecla premida repete o loop
+	JMP main_loop		; volta ao inicio
